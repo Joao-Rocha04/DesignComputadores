30bb814f65face057fd68b6f828ac4540cad109a
Joao-Rocha04 <joaoup30@gmail.com>
2025-04-26 12:03:20 -0300

    minhas mudanças para inclussão da label

--- AssemblerASM_BIN_VHDL/AssemblerASM_BIN_VHDL.py
+++ AssemblerASM_BIN_VHDL/AssemblerASM_BIN_VHDL.py
@@ -1,276 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-- Criado em 07/Fevereiro/2022
-- Atualizado em 19/04/2023
-
-@autor: Marco Mello e Paulo Santos
-
-
-Regras:
-
-1) O Arquivo ASM.txt não pode conter linhas iniciadas com caracteres ' ' ou '\n')
-2) Linhas somente com comentários são excluídas 
-3) Instruções sem comentário no arquivo ASM receberão como comentário no arquivo BIN a própria instrução
-4) Exemplo de codigo invalido:
-                            0.___JSR @14 #comentario1
-                            1.___#comentario2           << Invalido ( Linha somente com comentário )
-                            2.___                       << Invalido ( Linha vazia )
-                            3.___JMP @5  #comentario3
-                            4.___JEQ @9
-                            5.___NOP
-                            6.___NOP
-                            7.___                       << Invalido ( Linha vazia )
-                            8.___LDI $5                 << Invalido ( Linha iniciada com espaço (' ') )
-                            9.___ STA $0
-                            10.__CEQ @0
-                            11.__JMP @2  #comentario4
-                            12.__NOP
-                            13.__ LDI $4                << Invalido ( Linha iniciada com espaço (' ') )
-                            14.__CEQ @0
-                            15.__JEQ @3
-                            16.__#comentario5           << Invalido ( Linha somente com comentário )
-                            17.__JMP @13
-                            18.__NOP
-                            19.__RET
-                                
-5) Exemplo de código válido (Arquivo ASM.txt):
-                            0.___JSR @14 #comentario1
-                            1.___JMP @5  #comentario3
-                            2.___JEQ @9
-                            3.___NOP
-                            4.___NOP
-                            5.___LDI $5
-                            6.___STA $0
-                            7.___CEQ @0
-                            8.___JMP @2  #comentario4
-                            9.___NOP
-                            10.__LDI $4
-                            11.__CEQ @0
-                            12.__JEQ @3
-                            13.__JMP @13
-                            14.__NOP
-                            15.__RET
-                            
-6) Resultado do código válido (Arquivo BIN.txt):
-                            0.__tmp(0) := x"90E"; -- comentario1
-                            1.__tmp(1) := x"605"; -- comentario3
-                            2.__tmp(2) := x"709"; -- JEQ @9
-                            3.__tmp(3) := x"000"; -- NOP
-                            4.__tmp(4) := x"000"; -- NOP
-                            5.__tmp(5) := x"405"; -- LDI $5
-                            6.__tmp(6) := x"500"; -- STA $0
-                            7.__tmp(7) := x"800"; -- CEQ @0
-                            8.__tmp(8) := x"602"; -- comentario4
-                            9.__tmp(9) := x"000"; -- NOP
-                            10._tmp(10) := x"404"; -- LDI $4
-                            11._tmp(11) := x"800"; -- CEQ @0
-                            12._tmp(12) := x"703"; -- JEQ @3
-                            13._tmp(13) := x"60D"; -- JMP @13
-                            14._tmp(14) := x"000"; -- NOP
-                            15._tmp(15) := x"A00"; -- RET
-
+Assembler para arquitetura acumulador (INSPER) com suporte a labels e operandos em binário de 9 bits.
+Gera BIN.txt, onde cada instrução tem o opcode (hex, 1 dígito) e o operando (binário, 9 bits), mantendo o comentário original ao lado da instrução e anotando definições de labels.
 """
 
+import re
 
+# Arquivos de entrada e saída
+inputASM = 'ASM.txt'
+outputBIN = 'BIN.txt'
 
-inputASM = 'ASM.txt' #Arquivo de entrada de contém o assembly
-outputBIN = 'BIN.txt' #Arquivo de saída que contém o binário formatado para VHDL
-outputMIF = 'initROM.mif' #Arquivo de saída que contém o binário formatado para .mif
-
-noveBits = False
-
-#definição dos mnemônicos e seus
-#respectivo OPCODEs (em Hexadecimal)
-mne =	{ 
-       "NOP":   "0",
-       "LDA":   "1",
-       "SOMA":  "2",
-       "SUB":   "3",
-       "LDI":   "4",
-       "STA":   "5",
-       "JMP":   "6",
-       "JEQ":   "7",
-       "CEQ":   "8",
-       "JSR":   "9",
-       "RET":   "A",
+# Dicionário de mnemônicos para OPCODE (hexadecimal de 1 dígito)
+mne = {
+    "NOP": "0",
+    "LDA": "1",
+    "SOMA": "2",
+    "SUB": "3",
+    "LDI": "4",
+    "STA": "5",
+    "JMP": "6",
+    "JEQ": "7",
+    "CEQ": "8",
+    "JSR": "9",
+    "RET": "A",
 }
 
-#Converte o valor após o caractere arroba '@'
-#em um valor hexadecimal de 2 dígitos (8 bits)
-def  converteArroba(line):
-    line = line.split('@')
-    line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-    line = ''.join(line)
-    return line
-    
-#Converte o valor após o caractere arroba '@'
-#em um valor hexadecimal de 2 dígitos (8 bits) e...
-#concatena com o bit de habilita 
-def  converteArroba9bits(line):
-    line = line.split('@')
-    if(int(line[1]) > 255 ):
-        line[1] = str(int(line[1]) - 256)
-        line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '1' & x\"" + line[1]
-    else:
-        line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '0' & x\"" + line[1]
-    line = ''.join(line)
-    return line
- 
-#Converte o valor após o caractere cifrão'$'
-#em um valor hexadecimal de 2 dígitos (8 bits) 
-def  converteCifrao(line):
-    line = line.split('$')
-    line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-    line = ''.join(line)
-    return line
-
-#Converte o valor após o caractere arroba '$'
-#em um valor hexadecimal de 2 dígitos (8 bits) e...
-#concatena com o bit de habilita 
-def  converteCifrao9bits(line):
-    line = line.split('$')
-    if(int(line[1]) > 255 ):
-        line[1] = str(int(line[1]) - 256)
-        line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '1' & x\"" + line[1]
-    else:
-        line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '0' & x\"" + line[1]
-    line = ''.join(line)
-    return line
-        
-#Define a string que representa o comentário
-#a partir do caractere cerquilha '#'
-def defineComentario(line):
-    if '#' in line:
-        line = line.split('#')
-        line = line[0] + "\t#" + line[1]
-        return line
-    else:
-        return line
-
-#Remove o comentário a partir do caractere cerquilha '#',
-#deixando apenas a instrução
-def defineInstrucao(line):
-    line = line.split('#')
-    line = line[0]
-    return line
-    
-#Consulta o dicionário e "converte" o mnemônico em
-#seu respectivo valor em hexadecimal
-def trataMnemonico(line):
-    line = line.replace("\n", "") #Remove o caracter de final de linha
-    line = line.replace("\t", "") #Remove o caracter de tabulacao
-    line = line.split(' ')
-    line[0] = mne[line[0]]
-    line = "".join(line)
-    return line
-    
+# Regex para detectar labels (linhas como "label:" ou ".label:")
+label_pattern = re.compile(r'^(?P<label>\.?\w+):$')
 
 
-with open(inputASM, "r") as f: #Abre o arquivo ASM
-    lines = f.readlines() #Verifica a quantidade de linhas
-    
-    
-with open(outputBIN, "w+") as f:  #Abre o destino BIN
+def int_to_bin9(val: int) -> str:
+    """Converte inteiro [0,511] para string binária de 9 bits."""
+    if val < 0 or val > 511:
+        raise ValueError(f"Valor {val} fora do intervalo [0,511] para 9 bits.")
+    return format(val, '09b')
 
-    cont = 0 #Cria uma variável para contagem
-    
-    for line in lines:        
-        
-        #Verifica se a linha começa com alguns caracteres invalidos ('\n' ou ' ' ou '#')
-        if (line.startswith('\n') or line.startswith(' ') or line.startswith('#')):
-            line = line.replace("\n", "")
-            print("-- Sintaxe invalida" + ' na Linha: ' + ' --> (' + line + ')') #Print apenas para debug
-        
-        #Se a linha for válida para conversão, executa
+# 1ª passagem: coletar labels e suas posições (endereços)
+labels = {}
+pc = 0
+with open(inputASM, 'r') as f:
+    for raw in f:
+        line = raw.split('#', 1)[0].strip()
+        if not line:
+            continue
+        m = label_pattern.match(line)
+        if m:
+            name = m.group('label').lstrip('.')
+            labels[name] = pc
         else:
-            
-            #Exemplo de linha => 1. JSR @14 #comentario1
-            comentarioLine = defineComentario(line).replace("\n","") #Define o comentário da linha. Ex: #comentario1
-            instrucaoLine = defineInstrucao(line).replace("\n","") #Define a instrução. Ex: JSR @14
-            
-            instrucaoLine = trataMnemonico(instrucaoLine) #Trata o mnemonico. Ex(JSR @14): x"9" @14
-                  
-            if '@' in instrucaoLine: #Se encontrar o caractere arroba '@' 
-                if noveBits == False:
-                    instrucaoLine = converteArroba(instrucaoLine) #converte o número após o caractere Ex(JSR @14): x"9" x"0E"
-                else:
-                    instrucaoLine = converteArroba9bits(instrucaoLine) #converte o número após o caractere Ex(JSR @14): x"9" x"0E"
-                    
-            elif '$' in instrucaoLine: #Se encontrar o caractere cifrao '$'
-                if noveBits == False:
-                    instrucaoLine = converteCifrao(instrucaoLine) #converte o número após o caractere Ex(LDI $5): x"4" x"05"
-                else:
-                    instrucaoLine = converteCifrao9bits(instrucaoLine) #converte o número após o caractere Ex(LDI $5): x"4" x"05"
-                
-            else: #Senão, se a instrução nao possuir nenhum imediato, ou seja, nao conter '@' ou '$'
-                instrucaoLine = instrucaoLine.replace("\n", "") #Remove a quebra de linha
-                if noveBits == False:
-                    instrucaoLine = instrucaoLine + '00' #Acrescenta o valor x"00". Ex(RET): x"A" x"00"
-                else:
-                    instrucaoLine = instrucaoLine + "\" & " + "\'0\' & " + "x\"00" #Acrescenta o valor x"00". Ex(RET): x"A" x"00"
-                
-            
-            line = 'tmp(' + str(cont) + ') := x"' + instrucaoLine + '";\t-- ' + comentarioLine + '\n'  #Formata para o arquivo BIN
-                                                                                                       #Entrada => 1. JSR @14 #comentario1
-                                                                                                       #Saída =>   1. tmp(0) := x"90E";	-- JSR @14 	#comentario1
-                                        
-            cont+=1 #Incrementa a variável de contagem, utilizada para incrementar as posições de memória no VHDL
-            f.write(line) #Escreve no arquivo BIN.txt
-            
-            print(line,end = '') #Print apenas para debug
-            
+            pc += 1
 
-            
-############################             
-############################            
-#Conversão para arquivo .mif
-############################             
-############################
-            
-with open(outputMIF, "r") as f: #Abre o arquivo .mif
-    headerMIF = f.readlines() #Faz a leitura das linhas do arquivo,
-                              #para fazer a aquisição do header
-    
-    
-with open(outputBIN, "r") as f: #Abre o arquivo BIN
-    lines = f.readlines() #Faz a leitura das linhas do arquivo
-    
-    
-with open(outputMIF, "w") as f:  #Abre o destino .mif novamente
-                                 #agora para preenchê-lo com o pograma
-
-    cont = 0 #Cria uma variável para contagem
-    
-    #########################################
-    #Preenche com o header lido anteriormente 
-    for lineHeader in headerMIF:      
-        if cont < 21:           #Contagem das linhas de cabeçalho
-            f.write(lineHeader) #Escreve no arquivo se saída .mif o cabeçalho (21 linhas)
-        cont = cont + 1         #Incrementa varíavel de contagem
-   #-----------------------------------------
-   ##########################################
-  
-    for line in lines: #Varre as linhas do código formatado para a ROM (VHDL)
-    
-        replacements = [('t', ''), ('m', ''), ('p', ''), ('(', ''), (')', ''), ('=', ''), ('x', ''), ('"', '')] #Define os caracteres que serão excluídos
-        
-        ##########################################
-        #Remove os caracteres que foram definidos
-        for char, replacement in replacements:
-            if char in line:
-                line = line.replace(char, replacement)
-        #-----------------------------------------
-        ##########################################
-                
-        line = line.split('#') #Remove o comentário da linha
-        
-        if "\n" in line[0]:
-            line = line[0] 
+# 2ª passagem: gerar BIN.txt com opcode hex e operando binário, mantendo instrução + comentário
+pc = 0
+with open(inputASM, 'r') as f, open(outputBIN, 'w') as outf:
+    for raw in f:
+        raw_line = raw.rstrip('\n')
+        # separa instrução e comentário preservando instrução completa
+        if '#' in raw_line:
+            parts = raw_line.split('#', 1)
+            code = parts[0].strip()
+            comment_line = code + "\t#" + parts[1]
         else:
-            line = line[0] + '\n' #Insere a quebra de linha ('\n') caso não tenha
+            code = raw_line.strip()
+            comment_line = code
 
-        f.write(line) #Escreve no arquivo initROM.mif
-    f.write("END;") #Acrescente o indicador de finalização da memória. (END;)
+        # ignora vazias ou apenas comentários
+        if not code or code.startswith('#'):
+            continue
+
+        # detecta definição de label e anota no BIN
+        m = label_pattern.match(code)
+        if m:
+            name = m.group('label').lstrip('.')
+            addr = labels[name]
+            comment = f'Label {name} definida em endereço {addr}'
+            outf.write(f'-- {comment}\n')
+            print(f'-- {comment}\n', end='')
+            continue
+
+        # processa instrução normal
+        tokens = code.split()
+        mnemonic = tokens[0].upper()
+        if mnemonic not in mne:
+            raise KeyError(f"Mnêmônico desconhecido '{mnemonic}' na linha: {raw_line}")
+        opcode = mne[mnemonic]
+
+        # calcula valor do operando (imediato ou label)
+        if len(tokens) > 1:
+            oper = tokens[1]
+            if oper.startswith('@') or oper.startswith('$'):
+                num = int(oper[1:])
+            else:
+                name = oper.lstrip('.')
+                if name not in labels:
+                    raise KeyError(f"Label '{name}' não definido antes da linha: {raw_line}")
+                num = labels[name]
+        else:
+            num = 0
+        operand_bin = int_to_bin9(num)
+
+        # monta saída no formato VHDL
+        line_out = f'tmp({pc}) := x"{opcode}" & b"{operand_bin}";\t-- {comment_line}\n'
+        outf.write(line_out)
+        print(line_out, end='')
+        pc += 1
